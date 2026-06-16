b49b0d63c5e5ec811f6708c389d0569ba7fee4a0
survivorsystems <chickensweets87@gmail.com>
2026-06-15 21:41:37 -0500

    Remove cto sender and add RockApp favicon

--- convex/ResendOTP.ts
+++ convex/ResendOTP.ts
@@ -12,7 +12,7 @@ export const ResendOTP = Email({
   async sendVerificationRequest({ identifier: email, provider, token }) {
     const resend = new ResendAPI(provider.apiKey);
     const { error } = await resend.emails.send({
-      from: "RockApp <rockhoundapp@outlook.com>",
+      from: "RockApp <onboarding@resend.dev>",
       to: [email],
       subject: "Your sign-in code",
       text: `Your verification code is: ${token}`,
