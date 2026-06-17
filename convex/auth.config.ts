888c0a996607fd4cd1671a8f32b0b0d8e567a702
survivorsystems <chickensweets87@gmail.com>
2026-06-17 17:30:18 -0500

    Update domain configuration to use environment variable

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL ?? "https://tough-emu-800.convex.site",
+      domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
   ],
